370c8619ee15aeaa8300a37340281f62dc34d3a8
brandon025 <brandon025@gmail.com>
2016-11-29 13:40:41 -0800

    ttest

--- ololoo/prefix-lite/test/index.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-import './merge';
-
-const tests = fs.readdirSync(path.join(__dirname, 'fixes')).forEach((testFile) => {
-  require(path.join(__dirname, 'fixes', testFile));
-});
